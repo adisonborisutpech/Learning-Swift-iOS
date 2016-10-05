be0d97da23f273cc0c0410f6804b4f83f03c6974
Adison Borisutpech <adisonborisutpech@gmail.com>
2016-10-04 20:59:43 -0400

    Learning explicit/implicit variables and conversion

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,4 +8,23 @@ var myVariable = 42
 
 myVariable = 50
 
+
+// Swift est une langage pas très typer
+// Pour déclarer une constante
 let myConstant = 42
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+
+// Pour déclarer explicitemenr le type de la variable
+let explicitDouble: Double = 70
+
+// Essai
+var explicitFloat: Float = 4;
+
+// Une variable ne peut être implicitement
+let label = "The width is "
+let width = 94
+
+// Il faut montrer qu'on veut convertir "width" en String explicitement
+let widthLabel = label + String(width)
